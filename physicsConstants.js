1061ab953f5809d94223c6ef839c5beee8980cac
Brian W. Mulligan <bwmulligan@astronaos.com>
2020-05-20 17:10:40 -0500

    finish testing physical constants

--- physicsConstants.js
+++ physicsConstants.js
@@ -24,29 +24,29 @@ const kIrradianceSolar = 1.361e3; // J m^{-2} (IAU Resolution B3 2015)
 const kH0h = 0.726; // s^{-1} h // WMAP 7
 
 // semi-empircal derived constants
-const kESU = kChargeFundamental * kSpeedLight / 10.0; // esu, now defined in terms of speed of light and fundamental charge. //4.80320451e-10;
+const kESU = kChargeFundamental * kSpeedLight * 10.0; // esu, now defined in terms of speed of light and fundamental charge. //4.80320451e-10;
 const kAMU_kg = 1.0e-3 / kAvogadro; // AMU in kg
-const kMassProton = 1.007276466621 * kAMU_kg;//kg
-const kMassElectron = 5.48579909065e-4 * kAMU_kg;//kg (CODATA 2018)
+const kMassProton = 1.67262192369e-27;//kg
+const kMassElectron = 9.1093837015e-31;//kg (CODATA 2018)
 
 // derived constants
 // combined mass of Sun and Newton's gravitational constant
-const kGMSolar = 1.3271244e20; // cm^3 s^2 (IAU Resolution B3 2015)
+const kGMSolar = 1.3271244e20; // m^3 s^2 (IAU Resolution B3 2015)
 const kMassSolar = kGMSolar / kGravityNewton; // g (IAU Resolution B3 2015)
 
 // combined mass of Jupiter and Newton's gravitational constant
-const kGMJupiter = 1.2668653e17; // cm^3 s^2 (IAU Resolution B3 2015)
+const kGMJupiter = 1.2668653e17; // m^3 s^2 (IAU Resolution B3 2015)
 const kMassJupiter = kGMJupiter / kGravityNewton; // g (IAU Resolution B3 2015)
-const kRadiusJupiterPolar = 6.6854e7; // cm  (IAU Resolution B3 2015)
-const kRadiusJupiterEquatorial = 7.1492e7; // cm  (IAU Resolution B3 2015)
-const kRadiusJupiter = Math.pow(kRadiusJupiterPolar * kRadiusJupiterEquatorial * kRadiusJupiterEquatorial,1.0/3.0); // average radius for volume, cm
+const kRadiusJupiterPolar = 6.6854e7; // m  (IAU Resolution B3 2015)
+const kRadiusJupiterEquatorial = 7.1492e7; // m  (IAU Resolution B3 2015)
+const kRadiusJupiter = Math.pow(kRadiusJupiterPolar * kRadiusJupiterEquatorial * kRadiusJupiterEquatorial,1.0/3.0); // average radius for volume, m
 
 // combined mass of Jupiter and Newton's gravitational constant
-const kGMEarth = 3.986004e14; // cm^3 s^2 (IAU Resolution B3 2015)
+const kGMEarth = 3.986004e14; // m^3 s^2 (IAU Resolution B3 2015)
 const kMassEarth = kGMEarth / kGravityNewton; // g (IAU Resolution B3 2015)
-const kRadiusEarthPolar = 6.3568e6; // cm  (IAU Resolution B3 2015)
-const kRadiusEarthEquatorial = 6.3781e6; // cm  (IAU Resolution B3 2015)
-const kRadiusEarth = Math.pow(kRadiusEarthPolar * kRadiusEarthEquatorial * kRadiusEarthEquatorial,1.0/3.0); // average radius for volume, cm
+const kRadiusEarthPolar = 6.3568e6; // m  (IAU Resolution B3 2015)
+const kRadiusEarthEquatorial = 6.3781e6; // m  (IAU Resolution B3 2015)
+const kRadiusEarth = Math.pow(kRadiusEarthPolar * kRadiusEarthEquatorial * kRadiusEarthEquatorial,1.0/3.0); // average radius for volume, m
 
 const kParsec = kAstronomicalUnit * 648000.0 / Math.PI; // 1 pc in m
 const kMegaparsec = kParsec * 1.0e6; // 1 Mpc in m
@@ -64,18 +64,18 @@ const kVacuumPermeability = 1.0 / (kSpeedLight * kSpeedLight * kVacuumPermittivi
 
 const kRadiationDensity = 8.0 * Math.PI * Math.PI * Math.PI * Math.PI * Math.PI * kBoltzmann * kBoltzmann * kBoltzmann * kBoltzmann / (15.0 * khc * khc * khc); //7.5657e-15; // J m^{-3} K^{-4}
 const kStefanBoltzman = kRadiationDensity * kSpeedLight * 0.25;////cgs
-const kWein = khc / kBoltzmann / 4.965114231e2;// m
+const kWein = khc / kBoltzmann / 4.965114231;// m
 const kBoltzmann_eV = kBoltzmann / kChargeFundamental; // eV/K 
 const kPlanckReduced = kPlanck / (2.0 * Math.PI); // J s //@@
 const kPlanck_eV = kPlanck / kChargeFundamental;// eV s 
 const kPlanckReduced_eV = kPlanckReduced / kChargeFundamental;// eV s
-const khc_eVnm = kPlanckReduced_eV * kSpeedLight * 1e9;//eV nm
+const khc_eVnm = kPlanck_eV * kSpeedLight * 1e9;//eV nm
 const kH0 = kH0std * kH0h;
 const kFineStructure = kCoulomb * kChargeFundamental * kChargeFundamental / (khc) * 2.0 * Math.PI;
-const kRadiusBohr = kPlanckReduced * kPlanckReduced * kCoulomb / (kMassElectron * kChargeFundamental * kChargeFundamental); // m
-const kRydberg = (2.0 * Math.PI * Math.PI * kMassElectron * kESU * kESU * kESU * kESU) / (kPlanck * kPlanck);
+const kRadiusBohr = kPlanckReduced * kPlanckReduced / (kCoulomb * kMassElectron * kChargeFundamental * kChargeFundamental); // m
+const kRydberg = (kMassElectron * kChargeFundamental * kChargeFundamental * kChargeFundamental * kChargeFundamental) / (8.0 * kVacuumPermittivity * kVacuumPermittivity * kPlanck * kPlanck);
 const kRydberg_eV = kRydberg / kChargeFundamental;
 const kMassHydrogen = kMassElectron + kMassProton - kRydberg / (kSpeedLight * kSpeedLight);
-const kCrossSectionThomsonElectron = kChargeFundamental * kChargeFundamental * kChargeFundamental * kChargeFundamental * kCoulomb * kCoulomb / (kMassElectron * kMassElectron * kSpeedLight * kSpeedLight * kSpeedLight * kSpeedLight) * 8.0 * Math.PI / 3.0;
-const kCrossSectionThomsonProton = kChargeFundamental * kChargeFundamental * kChargeFundamental * kChargeFundamental * kCoulomb * kCoulomb / (kMassProton * kMassProton * kSpeedLight * kSpeedLight * kSpeedLight * kSpeedLight) * 8.0 * Math.PI / 3.0;
+const kCrossSectionThomsonElectron = (kChargeFundamental * kChargeFundamental / kMassElectron)**2 * 8.0e-14 * Math.acos(0.5); //Math.PI / 3.0;
+const kCrossSectionThomsonProton = kChargeFundamental * kChargeFundamental * kChargeFundamental * kChargeFundamental / (kMassProton * kMassProton) * 8.0e-14 * Math.PI / 3.0;
 
--- test/testPhysicalConstants.js
+++ test/testPhysicalConstants.js
@@ -3,15 +3,15 @@
 var iFailuresPhysicalConstants = new TestCount();
 
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"c",kSpeedLight,2.99792458e8));
-iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_e",kMassElectron,9.108980087382e-31));
-iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_p",kMassProton,1.672547813969e-27));
-iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_h",kMassHydrogen,1.673458687724e-27));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_e",kMassElectron,9.1093837015e-31));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_p",kMassProton,1.672621923e-27));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_h",kMassHydrogen,1.67353284e-27));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_Sun",kMassSolar,1.9884099e30));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_Earth",kMassEarth,5.972168e24));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_Jupiter",kMassJupiter,1.89812459e27));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"m_AMU",kAMU_kg,1.660465492239e-27));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"q_e",kChargeFundamental,1.602176634e-19));
-iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"r_Bohr",kRadiusBohr,5.29200659e-11));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"r_Bohr",kRadiusBohr,5.29177210903e-11));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"au",kAstronomicalUnit,1.495978707e11));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"pc",kParsec,3.085677581e16));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Mpc",kMegaparsec,3.085677581e22));
@@ -19,9 +19,9 @@ iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Gpc",kGigapars
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"R_sun",kRadiusSolar,6.957e8));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"R_earth",kRadiusEarth,6.370992e6));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"R_jupiter",kRadiusJupiter,6.9911308e7));
-iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Rydberg",kRydberg,2.17977577e-18));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Rydberg",kRydberg,2.1798723611035e-18));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"L_sun",kLuminositySolar,3.828e26));
-iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"k_Coulomb",kCoulomb,8.98755179e9));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Coulomb",kCoulomb,8.98755179e9));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Vacuum Permittivity",kVacuumPermittivity,8.8541878128e-12));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Vacuum Permeability",kVacuumPermeability,1.2566370621e-6));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Boltzmann",kBoltzmann,1.380649e-23));
@@ -32,6 +32,29 @@ iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Stefan-Boltzma
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Radiation Density",kRadiationDensity,7.565733e-16));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Fine Structure", kFineStructure,7.29735257e-3));
 iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Avogadro",kAvogadro,6.02240760e23));
+
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"H0",kH0std,3.24077928944e-18));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Age of Universe (H0) (Gy)",1.0e-9 / kH0std / kYear,9.778131308));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Age of Universe (h0) (Gy)",1.0e-9 / kH0 / kYear,13.4685004));
+
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"ly",kLightYear,9.4605281688e15));
+
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"hc",khc,1.98644582e-25));
+
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Wein",kWein,2.897771855e-3));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Thomson Cross Section (electron)",kCrossSectionThomsonElectron,6.6524587321e-29));
+iFailuresPhysicalConstants.addEq(testValueAndReport(bSilent_Pass,"Thomson Cross Section (proton)",kCrossSectionThomsonProton,1.97317048235e-35));
+
+
+/*
+const kBoltzmann_eV = kBoltzmann / kChargeFundamental; // eV/K 
+const kPlanckReduced = kPlanck / (2.0 * Math.PI); // J s //@@
+const kPlanck_eV = kPlanck / kChargeFundamental;// eV s 
+const kPlanckReduced_eV = kPlanckReduced / kChargeFundamental;// eV s
+const khc_eVnm = kPlanckReduced_eV * kSpeedLight * 1e9;//eV nm
+const kH0 = kH0std * kH0h;
+const kRydberg_eV = kRydberg / kChargeFundamental;
+*/
 
 iFailuresPhysicalConstants.report("Physics Constants");
 
